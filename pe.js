b5bf7941d2ffeb7681f3ab211eac0f75842843db
qzhan15 <qzhan15@appledeMacBook-Pro.local>
2018-04-30 14:00:11 +0800

    tmp
    Signed-off-by: qzhan15 <qzhan15@appledeMacBook-Pro.local>

---
+++ pe.js
@@ -0,0 +1,92 @@
+var fs = require("fs");
+var rl = require("readline");
+
+var id = process.argv[2];
+var r_inc1 = process.argv[3];
+var r_inc3 = process.argv[4];
+var p_inc1 = process.argv[5];
+var p_inc3 = process.argv[6];
+var file1 = "report/" + id + ".csv";
+var file2 = "ntes/" + id + ".cvs";
+
+var profile_ttm = undefined;
+var market_value = undefined;
+
+var profile = [];
+var Q = undefined;
+
+function loadData()
+{
+	var lineReader = rl.createInterface({
+		input: fs.createReadStream(file1),
+	});
+
+	var i=0;	
+	lineReader.on('line', function(line) {
+		if (line.startsWith("#"))
+			return;
+		var tokens = line.split(",");
+
+		profile.push(parseFloat(tokens[2]) * 10000);
+		if (!Q) {
+			date = tokens[0];
+			if (date.endsWith("12-31"))
+				Q = 4;
+			else if (date.endsWith("09-30"))
+				Q = 3;
+			else if (date.endsWith("06-30"))
+				Q = 2;
+			else
+				Q = 1;	
+		}
+		if (profile.length >= 5)
+			lineReader.close();
+	});
+
+	lineReader.on('close', function() {
+		profile_ttm = profile[0] + profile[Q] - profile[4];
+		getMarketValue();
+	});
+
+}
+
+function getMarketValue()
+{
+
+	var lineReader = rl.createInterface({
+		input: fs.createReadStream(file2),
+	});
+	
+	var row = 0;
+	lineReader.on('line', function(line) {
+
+		if (row == 1)
+		{
+			var tokens = line.split(",");
+			market_value = parseFloat(tokens[13]);
+			lineReader.close();
+		}
+		row++;
+	});
+
+	lineReader.on('close', function() {
+		display();	
+	});
+}
+
+function display()
+{
+	var pe = market_value / profile_ttm;
+	if (pe > 0)
+	console.log(id + " " + pe + " " + r_inc1  + " " + r_inc3 + " " + p_inc1 + " " + p_inc3 + " " + market_value + " " + profile_ttm); 
+}
+/*
+function displayHeader()
+{
+	console.log("#id price target_price rate ttm 2017 2018 2019");
+}
+*/
+
+//displayHeader();
+loadData();
+
